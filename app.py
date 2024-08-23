5e9da78f33994b1f4c0341f99abb5d49410750a0
Héctor <hcastc00@estudiantes.unileon.es>
2024-08-23 18:09:35 +0200

    bugfix

--- app.py
+++ app.py
@@ -23,19 +23,28 @@ def move_old_images_to_date_folder():
             file_extension = os.path.splitext(filename)[1]
             
             # Crear el nuevo nombre de archivo basado en la hora actual
-            new_filename = datetime.now().strftime('%H-%M-%S') + file_extension
+            timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S_%f')[:-3]
+            new_filename = f"{timestamp}{file_extension}"
             
             # Mover el archivo a la carpeta del día con el nuevo nombre
             new_file_path = os.path.join(date_folder, new_filename)
             os.rename(file_path, new_file_path)
 
-
 @app.route('/pantalla')
 def index():
-    # Siempre mostrar currentIMG
-    current_image = os.path.join(app.config['UPLOAD_FOLDER'], 'currentIMG')
-    if os.path.exists(current_image):
-        return render_template('index.html', image='currentIMG')
+    # Buscar el archivo currentIMG con cualquier extensión en el directorio de uploads
+    upload_folder = app.config['UPLOAD_FOLDER']
+    current_image_filename = None
+
+    # Iterar a través de los archivos en el directorio de uploads
+    for filename in os.listdir(upload_folder):
+        if filename.startswith('currentIMG') and os.path.isfile(os.path.join(upload_folder, filename)):
+            current_image_filename = filename
+            break
+
+    # Renderizar la plantilla y pasar el nombre completo del archivo
+    if current_image_filename:
+        return render_template('index.html', image=current_image_filename)
     else:
         return render_template('index.html', image=None)
 
@@ -51,15 +60,16 @@ def upload_image():
             return redirect(url_for('upload_image'))
 
         if file:
-            # Mover imágenes antiguas a la carpeta de la fecha actual
+            # Mover las imágenes existentes a la carpeta de la fecha actual
             move_old_images_to_date_folder()
             
             # Guardar la nueva imagen como currentIMG
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'currentIMG')
+            file_extension = os.path.splitext(file.filename)[1]
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'currentIMG' + file_extension)
             file.save(file_path)
             
             # Notificar a todos los clientes para que recarguen la página
-            socketio.emit('image_uploaded', {'image': 'currentIMG'})
+            socketio.emit('image_uploaded', {'image': 'currentIMG' + file_extension})
             return "Imagen subida", 200
 
     return render_template('upload.html')
